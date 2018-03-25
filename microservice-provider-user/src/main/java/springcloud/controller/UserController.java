7788e5e17780d802b96f43d4fc9bc1571ea592ab
myoldfycy318 <myoldfycy318@126.com>
2018-03-25 22:26:05 +0800

    springcloud-docker

---
+++ microservice-provider-user/src/main/java/springcloud/controller/UserController.java
@@ -0,0 +1,31 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import springcloud.dao.UserRepository;
+import springcloud.domain.User;
+
+import java.util.Optional;
+
+/**
+ * UserController
+ *
+ * @author Zhang ShanMin
+ * @date 2018/3/24
+ * @time 11:13
+ */
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.get();
+    }
+
+}
+
