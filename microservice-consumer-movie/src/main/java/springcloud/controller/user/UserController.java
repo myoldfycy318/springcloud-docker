c2d4991319cabea5ba6a6b95e9c4d6b5fc3a18fe
myoldfycy318 <myoldfycy318@126.com>
2018-03-26 19:48:08 +0800

    docker

--- microservice-consumer-movie/src/main/java/springcloud/controller/user/UserController.java
+++ microservice-consumer-movie/src/main/java/springcloud/controller/user/UserController.java
@@ -1,33 +1,23 @@
-package springcloud.controller;
+package springcloud.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springcloud.dao.UserRepository;
+import org.springframework.web.client.RestTemplate;
 import springcloud.domain.User;
 
-import java.util.Optional;
-
-/**
- * UserController
- *
- * @author Zhang ShanMin
- * @date 2018/3/24
- * @time 11:13
- */
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
 
-    @GetMapping("/{id}")
-    public User findById(@PathVariable Long id) {
-        Optional<User> user = this.userRepository.findById(id);
-        return user.get();
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("{id}")
+    public User findById(@PathVariable Long id){
+        return restTemplate.getForObject("http://localhost:8000/user/"+id,User.class);
     }
 
 }
-
